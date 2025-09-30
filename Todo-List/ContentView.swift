005ddff5dc09e7f0172d6bc4c557b7eca12c1157
Eslam Gamal <eslam.gamal@exas2030.com>
2025-09-30 14:05:04 +0300

    Initial Commit

---
+++ Todo-List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo-List
+//
+//  Created by Eslam Gamal on 30/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo-List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo-List
+//
+//  Created by Eslam Gamal on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
